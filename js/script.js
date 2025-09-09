c2ebf5f34eabdf973fc121910ccb2d12c915b526
Carobel <cjkusee@gmail.com>
2025-09-09 19:53:06 +0200

    create pixels with id and class

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 SIZE = 16;
 
+// create a canvas of pixels
 function createPixels() {
     canvas = document.querySelector('#canvas');
     for(let x = 0; x < SIZE; x++) {
         for(let y = 0; y < SIZE; y++) {
             console.log(x,y);
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            pixel.id = `${String(x)},${String(y)}`
+            canvas.appendChild(pixel);
         }
     }
 }
