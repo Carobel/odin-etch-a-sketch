f4c2f3b400e3a6c1ab5bf71ca78af81a1f46419a
Carobel <cjkusee@gmail.com>
2025-09-09 21:27:23 +0200

    add rows of pixels

--- js/script.js
+++ js/script.js
@@ -4,12 +4,15 @@ SIZE = 16;
 function createPixels() {
     canvas = document.querySelector('#canvas');
     for(let x = 0; x < SIZE; x++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        canvas.appendChild(row);
+        
         for(let y = 0; y < SIZE; y++) {
-            console.log(x,y);
             const pixel = document.createElement('div');
             pixel.classList.add('pixel');
             pixel.id = `${String(x)},${String(y)}`
-            canvas.appendChild(pixel);
+            row.appendChild(pixel);
         }
     }
 }
